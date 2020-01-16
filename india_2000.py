e91d42094e87d06117a56fa451be027e2e67fcee
RobinDevillers <re.devillers1999@gmail.com>
2020-01-16 12:00:02 +0100

    Selected only India, with only 1 country_id

--- india_2000.py
+++ india_2000.py
@@ -6,11 +6,11 @@ with open('india_200.json', encoding= 'utf-16') as file:
 #first select for the country India
 india_incidents = []
 for observation in data:
-    india_incidents.append(observation['country_id'])
     if observation['country'] ==  'India':
         india_incidents.append(observation['country_id'])
 
 print(set(india_incidents))
 
+
 #select countries with a different country id
-#700, 100, 438, 770, 145
+#770, 771, 100, 775, 780, 433, 145, 438, 700
