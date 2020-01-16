52f0ffeb19aada5bc0328429005e186ffe80e185
RobinDevillers <re.devillers1999@gmail.com>
2020-01-16 11:58:04 +0100

    Selecting countries with the same id and country name

--- india_2000.py
+++ india_2000.py
@@ -1,3 +1,16 @@
-with open('conflict_data_full_lined.json') as file:
-    data = file.readlines()
+import json
 
+with open('india_200.json', encoding= 'utf-16') as file:
+    data = json.load(file)
+
+#first select for the country India
+india_incidents = []
+for observation in data:
+    india_incidents.append(observation['country_id'])
+    if observation['country'] ==  'India':
+        india_incidents.append(observation['country_id'])
+
+print(set(india_incidents))
+
+#select countries with a different country id
+#700, 100, 438, 770, 145
