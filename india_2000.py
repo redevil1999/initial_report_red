8a1ac2fc83fcd1f0fc58e75749ff4c7dbdf26fba
RobinDevillers <re.devillers1999@gmail.com>
2020-01-16 14:21:17 +0100

    Added comments for clarification

--- india_2000.py
+++ india_2000.py
@@ -12,7 +12,9 @@ for observation in data:
             india_list.remove(observation)
 
 
-
+#as the best estimate seems the most reliable estimate for the fatalities, it makes most sense to have those in the csv file
+#As the goal of this report is to calculate the number of fatalities in India in 2000, there is no reason to include any other variables
+#(especially because the file is already selected for India and the year 2000)
 with open('india_fatalities.csv', 'w', encoding= 'utf8') as file:
     file.write('fatalities\n')
     for entry in india_list:
