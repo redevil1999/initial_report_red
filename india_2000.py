cda4eb25600346afbfa8a9357072e98cae6da8b7
RobinDevillers <re.devillers1999@gmail.com>
2020-01-17 09:29:10 +0100

    Created 2 CSV files including fatalities and type of violence

--- india_2000.py
+++ india_2000.py
@@ -11,18 +11,27 @@ for observation in data:
         if observation['year'] <= int(2001):
             india_list_1989_2001.append(observation)
         else:
-            india_list_2001_2015.append(observation)
+            india_list_2002_2015.append(observation)
 
 
 
 #as the best estimate seems the most reliable estimate for the fatalities, it makes most sense to have those in the csv file
-#As the goal of this report is to calculate the number of fatalities in India in 2000, there is no reason to include any other variables
-#(especially because the file is already selected for India and the year 2000)
-# with open('india_fatalities.csv', 'w', encoding= 'utf8') as file:
-#     file.write('fatalities, type of violence\n')
-#     for entry in india_list:
-#         fatalities = {
-#             'deaths': entry['best'],
-#             'Type of violence': entry['type_of_violence']
-#         }
-#         file.write(f"{fatalities['deaths']}, {fatalities['Type of violence']}\n")
+#The aim is to compare the fatalities per type of violence of the first half of the dataset to the second half of the dataset,
+#Therefore, 2 datasets will be made that include both fatalities and types of violence.
+with open('india_1989_2001.csv', 'w', encoding= 'utf8') as file:
+    file.write('fatalities, type of violence\n')
+    for entry1 in india_list_1989_2001:
+        fatalities1 = {
+            'deaths': entry1['best'],
+            'Type of violence': entry1['type_of_violence']
+        }
+        file.write(f"{fatalities1['deaths']}, {fatalities1['Type of violence']}\n")
+
+with open('india_2002_2015.csv', 'w', encoding= 'utf8') as file:
+    file.write('fatalities, type of violence\n')
+    for entry2 in india_list_2002_2015:
+        fatalities2 = {
+            'deaths': entry2['best'],
+            'Type of violence': entry2['type_of_violence']
+        }
+        file.write(f"{fatalities2['deaths']}, {fatalities2['Type of violence']}\n")
