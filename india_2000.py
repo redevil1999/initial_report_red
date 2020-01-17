2186d57989272f58028b7934f6ca3f827ec9e621
RobinDevillers <re.devillers1999@gmail.com>
2020-01-17 09:37:29 +0100

    changed the name

--- india_2000.py
+++ india_2000.py
@@ -5,7 +5,7 @@ with open('india_200.json', encoding= 'utf-16') as file:
 
 #first select for the country India
 india_list_1989_2001 = []
-india_list_2001_2015 = []
+india_list_2002_2015 = []
 for observation in data:
     if observation['country'] ==  'India':
         if observation['year'] <= int(2001):
@@ -19,7 +19,7 @@ for observation in data:
 #The aim is to compare the fatalities per type of violence of the first half of the dataset to the second half of the dataset,
 #Therefore, 2 datasets will be made that include both fatalities and types of violence.
 with open('india_1989_2001.csv', 'w', encoding= 'utf8') as file:
-    file.write('fatalities, type of violence\n')
+    file.write('fatalities, type_of_violence\n')
     for entry1 in india_list_1989_2001:
         fatalities1 = {
             'deaths': entry1['best'],
@@ -28,7 +28,7 @@ with open('india_1989_2001.csv', 'w', encoding= 'utf8') as file:
         file.write(f"{fatalities1['deaths']}, {fatalities1['Type of violence']}\n")
 
 with open('india_2002_2015.csv', 'w', encoding= 'utf8') as file:
-    file.write('fatalities, type of violence\n')
+    file.write('fatalities, type_of_violence\n')
     for entry2 in india_list_2002_2015:
         fatalities2 = {
             'deaths': entry2['best'],
