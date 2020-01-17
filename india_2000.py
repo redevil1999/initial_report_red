2c026aa80410d27a31ace2aae9617cbea1ba8297
RobinDevillers <re.devillers1999@gmail.com>
2020-01-17 09:21:36 +0100

    Created 2 lists, 1 of the  years before and 1 of the years after 2001

--- india_2000.py
+++ india_2000.py
@@ -4,21 +4,25 @@ with open('india_200.json', encoding= 'utf-16') as file:
     data = json.load(file)
 
 #first select for the country India
-india_list = []
+india_list_1989_2001 = []
+india_list_2001_2015 = []
 for observation in data:
     if observation['country'] ==  'India':
-        india_list.append(observation)
-        if observation['year'] != int(2000):
-            india_list.remove(observation)
+        if observation['year'] <= int(2001):
+            india_list_1989_2001.append(observation)
+        else:
+            india_list_2001_2015.append(observation)
+
 
 
 #as the best estimate seems the most reliable estimate for the fatalities, it makes most sense to have those in the csv file
 #As the goal of this report is to calculate the number of fatalities in India in 2000, there is no reason to include any other variables
 #(especially because the file is already selected for India and the year 2000)
-with open('india_fatalities.csv', 'w', encoding= 'utf8') as file:
-    file.write('fatalities\n')
-    for entry in india_list:
-        fatalities = {
-            'deaths': entry['best']
-        }
-        file.write(f"{fatalities['deaths']}\n")
+# with open('india_fatalities.csv', 'w', encoding= 'utf8') as file:
+#     file.write('fatalities, type of violence\n')
+#     for entry in india_list:
+#         fatalities = {
+#             'deaths': entry['best'],
+#             'Type of violence': entry['type_of_violence']
+#         }
+#         file.write(f"{fatalities['deaths']}, {fatalities['Type of violence']}\n")
